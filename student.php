32aa0cb1e760384cef41274aa4a73e1cfb3eaffa
Ankit Kumar <emankitz029@gmail.com>
2023-03-07 09:29:12 +0530

    Initial -Commit #46876

---
+++ student.php
@@ -0,0 +1,36 @@
+
+<?php
+ 
+// GET
+// POST
+// REQUEST
+ 
+$name = $_REQUEST['student_name'];
+$class=  $_REQUEST['student_class'];
+$roll= $_REQUEST['student_roll'];
+//$class=  $_REQUEST['student_class']
+$mobile = $_REQUEST['student_mobile'];
+$password = $_REQUEST['student_password'];
+//$address = $_REQUEST['student_password'];
+
+
+ 
+echo $name;
+ 
+echo '<hr>';
+ 
+echo $class;
+ 
+echo '<hr>';
+ 
+echo $roll;
+ 
+echo '<hr>';
+ 
+echo $mobile;
+ 
+echo '<hr>';
+ 
+echo $password;
+ 
+?>
